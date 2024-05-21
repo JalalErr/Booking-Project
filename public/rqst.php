340598c42f0d8c1a32611e4ee7223a007d88587c
JalalErr <jalal.errajy@gmail.com>
2024-05-21 19:30:34 +0100

    creating The Login Page

---
+++ public/rqst.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $mail = $_POST['email'];
+        $pass = $_POST['password'];
+        echo "email : " .  $mail . "<br>";
+        echo "password: " . $pass;
+    ?>
+</body>
+</html>
